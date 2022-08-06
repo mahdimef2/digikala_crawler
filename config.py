d034bec214e71388c046937d5adbc74bc5290fe6
mahdimef <mahdieshghi201313@gmail.com>
2022-08-06 09:52:45 +0430

    add some changes for saving data in json file, now we can get data of all 100 page in website

--- config.py
+++ config.py
@@ -7,5 +7,8 @@ URL = BASE_URL + CATEGORY_URL
 
 # utils functions for scraping data from the site
 def convert_to_json(dict_data):
-    with open('products.json', 'w') as file:
-        json.dump(dict_data, file, indent=4, ensure_ascii=False)
+    with open('products.json') as file_open:
+        main_file = json.load(file_open)
+        main_file['products'].append(dict_data)
+        with open('products.json', 'w') as file_write:
+            json.dump(main_file, file_write, indent=4, ensure_ascii=False)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from time import sleep
 
 from selenium import webdriver
@@ -10,44 +11,47 @@ options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument('window-size=1200x600')
 driver = webdriver.Chrome(options=options)
-driver.get(URL)
-source = driver.page_source
 
-SCROLL_DOWN = False
-# scroll down to bottom for loading more products (max 60-80 products) almost page 4-5
-# html is for scrolling down to bottom
-if SCROLL_DOWN:
-    html = driver.find_element(By.TAG_NAME, 'html')
-    for product in range(0, 20):
-        html.send_keys(Keys.PAGE_DOWN)
-        sleep(1)
-all_product = driver.find_elements(By.XPATH, value='//*[@id="plpLayoutContainer"]/section[1]/div[2]/div[1]/div/div/a')
-
-# counts of all products in the page
-print(len(all_product))
-print(100 * '-')
-
-counter = 0  # counter for product
+# SCROLL_DOWN = False
+# # scroll down to bottom for loading more products (max 60-80 products) almost page 4-5
+# # html is for scrolling down to bottom
+# if SCROLL_DOWN:
+#     html = driver.find_element(By.TAG_NAME, 'html')
+#     for product in range(0, 20):
+#         html.send_keys(Keys.PAGE_DOWN)
+#         sleep(1)
+# create a new empty json file for the products
 dict_data = {'products': []}  # dictionary for data
+with open('products.json', 'w') as file:
+    json.dump(dict_data, file, indent=4, ensure_ascii=False)
 
-for product in all_product:
-    counter += 1
-    title = product.find_element(By.TAG_NAME, 'h2').text
-    # I use counter for get the price of the product because
-    # I can`t get the price of the product with the xpath of the "all_product"
-    price = driver.find_element(By.XPATH,
-                                f'//*[@id="plpLayoutContainer"]/section[1]/div[2]/div[1]/div/div[{counter}]'
-                                f'/a/article/div/div/div[4]/div[1]').text
-    link = product.get_attribute('href')
+# get all products in 100 page of website
+product_counter = 0  # counter for product 1 to n in all page
+for page in range(1, 5):
+    page_link = f'?page={page}'
+    driver.get(URL + page_link)
+    print(f'Scraping page {URL}{page_link}')
+    all_product = driver.find_elements(By.XPATH,
+                                       value='//*[@id="plpLayoutContainer"]/section[1]/div[2]/div[1]/div/div/a')
+    item_counter = 0  # counter for product 1-20 in any page that we are in
+    for product in all_product:
+        product_counter += 1
+        item_counter += 1
+        title = product.find_element(By.TAG_NAME, 'h2').text
+        # I use counter for get the price of the product because
+        # I can`t get the price of the product with the xpath of the "all_product"
+        price = driver.find_element(By.XPATH,
+                                    f'//*[@id="plpLayoutContainer"]/section[1]/div[2]/div[1]/div/div[{item_counter}]'
+                                    f'/a/article/div/div/div[4]/div[1]').text
+        link = product.get_attribute('href')
 
-    print(title, '\n', price, '\n', link)
-    print(100 * '-')
+        # save on dictionary and json file
+        dict_product_detail = {'Number': product_counter, 'title': title, 'price': price, 'link': link}
+        convert_to_json(dict_product_detail)
 
-    # save on dictionary
-    dict_data['products'].append({'Number': counter, 'title': title, 'price': price, 'link': link})
+    print(f'Scraping page {URL}{page_link} is DONE !')
+    # dict_data['products'] = []
 
-# save on json file
-convert_to_json(dict_data)
 driver.quit()
 
-# todo:get all pages
+# todo:add sqlite database for products
